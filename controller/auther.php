6039f43d78592146ec10e2360273d9f313264265
omar switi <omarswiti10@gmail.com>
2025-05-19 22:13:14 +0300

    s

--- controller/auther.php
+++ controller/auther.php
@@ -1,8 +1,23 @@
 <?php
-require_once 'config.php';
+require_once '/../config/config.php';
+require_once 'models/news.php'
 
 $sql = "SELECT * FROM news";
 $result = $conn->query($sql);
+
+class newsController{
+    private $model;
+
+    public function __construct($db) {
+        $this->model = new NewsModel($db);
+    }
+
+    public function displayNews() {
+        $result = $this->model->getAllNews();
+        include __DIR__ . '/../view/news_table.php';
+    }
+
+}
 
 echo "<h2>جميع الأخبار</h2>";
 
--- controller/editor_dashboard.php
+++ controller/editor_dashboard.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config.php';
+require_once '/../config/config.php';
 require_once 'models/news.php';
 require_once 'models/user.php';
 // جلب الأخبار من الأحدث إلى الأقدم
