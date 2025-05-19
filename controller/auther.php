bc9f00adbb1e34659a4e64c8203163c403b4cb40
omar switi <omarswiti10@gmail.com>
2025-05-19 22:43:04 +0300

    s

--- controller/auther.php
+++ controller/auther.php
@@ -1,5 +1,6 @@
 <?php
-require_once '/../config/config.php';
+<?php
+require_once __DIR__ . '/../config/config.php';
 require_once 'models/news.php'
 
 $sql = "SELECT * FROM news";
@@ -18,41 +19,5 @@ class newsController{
     }
 
 }
-
-echo "<h2>جميع الأخبار</h2>";
-
-if ($result && $result->num_rows > 0) {
-    echo "<table border='1'>";
-    echo "<tr>
-            <th>id</th>
-            <th>title</th>
-            <th>body</th>
-            <th>image</th>
-            <th>dateposted</th>
-            <th>category_id</th>
-            <th>author_id</th>
-            <th>status</th>
-          </tr>";
-
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . $row['title'] . "</td>";
-        echo "<td>" . $row['body'] . "</td>";
-        echo "<td><img src='" . $row['image'] . "' width='80'></td>";
-        echo "<td>" . $row['dateposted'] . "</td>";
-        echo "<td>" . $row['category_id'] . "</td>";
-        echo "<td>" . $row['author_id'] . "</td>";
-        echo "<td>" . $row['status'] . "</td>";
-        echo "</tr>";
-        
-    }
-
-    echo "</table>";
-} else {
-    echo "لا توجد أخبار لعرضها.";
-}
-
-echo "<button onclick=\"window.location.href='page_add_news.html'\">إضافة خبر جديد</button>";
 
 ?>
